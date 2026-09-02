71e16b265298fa8620cd7bb919484fb075b56098
mazen12moza-sketch <mazen12moza@gmail.com>
2026-09-03 02:23:35 +0300

    Update SHELL_CACHE version to noor-shell-v25

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // "noor-audio-v1") through explicit user actions (the "Download the full
 // Quran" button, "Download this surah audio", etc).
 
-const SHELL_CACHE = 'noor-shell-v24';
+const SHELL_CACHE = 'noor-shell-v25';
 const SHELL_FILES = [
   './',
   './index.html',
